58b5bc1e77e8188dcd2acde6a446de95bbb6c608
Alexander Zhu <ayzhu@princeton.edu>
2025-06-09 17:09:11 -0500

    added some backtesting data

--- pull_data.py
+++ pull_data.py
@@ -1,3 +1,4 @@
+from this import d
 import pandas as pd
 import numpy as np
 import os
@@ -163,6 +164,24 @@ def get_starting_pitchers(days_offset=0):
         pitchers.append([away_pitcher, 'home', game['game_id']])
     return pitchers
 
+def get_starting_pitchers_date(date):
+    today = date.strftime('%Y-%m-%d')
+
+    games = statsapi.schedule(start_date=today, end_date=today)
+
+    pitchers = []
+
+    # Loop through games and extract probable pitchers
+    for game in games:
+        home_team = game['home_name']
+        away_team = game['away_name']
+        home_pitcher = game.get('home_probable_pitcher', 'TBD')
+        away_pitcher = game.get('away_probable_pitcher', 'TBD')
+        
+        pitchers.append([home_pitcher, 'away', game['game_id']])
+        pitchers.append([away_pitcher, 'home', game['game_id']])
+    return pitchers
+
 def get_starting_ids(days_offset=0):
     todays_pitchers = get_starting_pitchers(days_offset=days_offset)
     print([p[0] for p in todays_pitchers])
@@ -181,13 +200,27 @@ def get_starting_ids(days_offset=0):
             pitcher_ids.append([player_id, p[1], p[2]])  # Append player_id, team, and game_id
     return pitcher_ids
 
+
+def get_starting_ids_date(date):
+    todays_pitchers = get_starting_pitchers_date(date)
+    pitcher_ids = []
+
+    for p in todays_pitchers:
+        if p[0] != '':
+            first_name, last_name = p[0].split(' ')[0], ' '.join(p[0].split(' ')[1:])
+            player_id = playerid_lookup(last_name, first_name, fuzzy=True)
+            player_id = player_id['key_mlbam'].values[0]
+            pitcher_ids.append([player_id, p[1], p[2]])  # Append player_id, team, and game_id
+    return pitcher_ids
+
 def get_barrel_percentage(player_id, start_date, end_date):
     player_barrels = statcast_pitcher(start_dt=start_date, end_dt=end_date,player_id=player_id)
 
     if player_barrels.empty:
         return {'left_pct': 0, 'right_pct': 0, 'total_pct': 0}
     # print(player_barrels['player_name'].iloc[0])
-    player_bbe = player_barrels[(player_barrels['description'].isin(['hit_into_play']))].copy()
+    player_bbe = player_barrels[player_barrels['events'].notna()].copy()
+    player_bbe = player_bbe[~player_bbe['events'].isin(['walk','hit_by_pitch','sac_fly','sac_bunt', 'truncated_pa'])]
     player_bbe = code_barrel(player_bbe)
     left_barrels, left_bbes = barrels_by_side(player_bbe, 'L')
     right_barrels, right_bbes = barrels_by_side(player_bbe, 'R')
@@ -222,7 +255,8 @@ def hitter_barrel_percentage(player_id, start_date, end_date, pitch_mix, sides):
             temp_barrel_dict = {'total_barrels': 0, 'total_bbes': 0, 'barrel_pct': 0}
         else:
             player_abs = player_barrels[(player_barrels['events'].notna()) & (player_barrels['stand'] == side)].copy()
-            player_abs = player_abs[~player_abs['events'].isin(['walk','hit_by_pitch','field_error','sac_fly','sac_bunt'])]
+            total_pas = player_abs.shape[0]
+            player_abs = player_abs[~player_abs['events'].isin(['walk','hit_by_pitch','field_error','sac_fly','sac_bunt','truncated_pa'])]
             player_abs['bases'] = player_abs['events'].map(lambda x: mapping.get(x, 0))
             player_hits = len(player_abs[player_abs['bases'] != 0]  )
             player_bbe = player_barrels[(player_barrels['description'].isin(['hit_into_play'])) & (player_barrels['stand'] == side)].copy()
@@ -235,10 +269,50 @@ def hitter_barrel_percentage(player_id, start_date, end_date, pitch_mix, sides):
             total_abs = player_abs.shape[0]
             temp_barrel_dict = {'total_barrels': total_barrels,
                         'total_bbes': total_bbes,
-                        'barrel_pct': total_barrels / total_bbes if total_bbes > 0 else 0,
+                        'total_abs': total_abs,
+                        'total_pas': total_pas,
+                        'barrel_pct (BBE)': total_barrels / total_bbes if total_bbes > 0 else 0,
+                        'barrel_pct (PA)': total_barrels / total_pas if total_abs > 0 else 0,
                         'blast_pct': player_bbe['is_blast'].sum() / total_bbes if total_bbes > 0 else 0,
                         'pulled_pct': player_bbe['pulled_air'].sum() / total_bbes if total_bbes > 0 else 0,
-                        'iso': (player_abs['bases'].sum() - player_hits)/ total_abs if total_abs > 0 else 0}
+                        'iso': (player_abs['bases'].sum() - player_hits)/ total_abs if total_abs > 0 else 0,
+                        'stand': side}
+        barrel_dict[side] = temp_barrel_dict
+    return barrel_dict
+
+
+def hitter_barrel_percentage_date(player_id, start_date, end_date, pitch_mix, sides, hitter_df):
+    mapping = {'single': 1, 'double': 2, 'triple': 3, 'home_run': 4}
+    player_barrels = hitter_df[player_id][(hitter_df[player_id]['game_date'] <= end_date) & (hitter_df[player_id]['game_date'] >= start_date)]
+    barrel_dict = {}
+
+    for side in sides:
+        if player_barrels.empty:
+            temp_barrel_dict = {'total_barrels': 0, 'total_bbes': 0, 'barrel_pct': 0}
+        else:
+            player_abs = player_barrels[(player_barrels['events'].notna()) & (player_barrels['stand'] == side)].copy()
+            total_pas = player_abs.shape[0]
+            player_abs = player_abs[~player_abs['events'].isin(['walk','hit_by_pitch','field_error','sac_fly','sac_bunt','truncated_pa'])]
+            player_abs['bases'] = player_abs['events'].map(lambda x: mapping.get(x, 0))
+            player_hits = len(player_abs[player_abs['bases'] != 0]  )
+            player_bbe = player_barrels[(player_barrels['description'].isin(['hit_into_play'])) & (player_barrels['stand'] == side)].copy()
+            player_bbe = code_barrel(player_bbe)
+            player_bbe['is_blast'] = player_bbe.apply(lambda row: get_blast(row['launch_speed'], row['bat_speed'],row['release_speed']), axis=1)
+            player_bbe['pulled_air'] = player_bbe.apply(lambda row: 1 if (spray_angle(row['hc_x'], row['hc_y'], row['stand']) < -15 and row['launch_angle'] >= 10) else 0, axis=1)
+            # player_bbe = player_bbe[player_bbe['pitch_name'].isin(pitch_mix[side].keys())]
+            total_barrels = player_bbe['is_barrel'].sum()
+            total_bbes = player_bbe.shape[0]
+            total_abs = player_abs.shape[0]
+            temp_barrel_dict = {'total_barrels': total_barrels,
+                        'total_bbes': total_bbes,
+                        'total_abs': total_abs,
+                        'total_pas': total_pas,
+                        'barrel_pct (BBE)': total_barrels / total_bbes if total_bbes > 0 else 0,
+                        'barrel_pct (PA)': total_barrels / total_pas if total_abs > 0 else 0,
+                        'blast_pct': player_bbe['is_blast'].sum() / total_bbes if total_bbes > 0 else 0,
+                        'pulled_pct': player_bbe['pulled_air'].sum() / total_bbes if total_bbes > 0 else 0,
+                        'iso': (player_abs['bases'].sum() - player_hits)/ total_abs if total_abs > 0 else 0,
+                        'stand': side}
         barrel_dict[side] = temp_barrel_dict
     return barrel_dict
 
@@ -246,7 +320,7 @@ def get_barrel_pcts(barrel_dict):
     barrel_pcts = []
     for side in barrel_dict:
         if barrel_dict[side]['total_bbes'] > 0:
-            barrel_pcts.append([barrel_dict[side]['barrel_pct'], barrel_dict[side]['blast_pct'], barrel_dict[side]['pulled_pct'], barrel_dict[side]['iso']])
+            barrel_pcts.append([barrel_dict[side]['barrel_pct (BBE)'], barrel_dict[side]['barrel_pct (PA)'], barrel_dict[side]['blast_pct'], barrel_dict[side]['pulled_pct'], barrel_dict[side]['iso'], barrel_dict[side]['stand'], barrel_dict[side]['total_bbes']])
     return barrel_pcts
 
 def check_barrel_pcts(barrel_pcts):
@@ -261,14 +335,14 @@ def check_barrel_pcts(barrel_pcts):
     """
     # print(type(barrel_pcts))
     if len(barrel_pcts) == 0:
-        return [0, 0, 0,0]
+        return [0, 0, 0,0,0,0, 0]
     elif len(barrel_pcts) == 1:
         if barrel_pcts[0][0] > 0.1:
             return barrel_pcts[0]
         else:
-            return [0, 0, 0,0]
+            return [0, 0, 0,0,0,0, 0]
     if barrel_pcts[0][0] < 0.1 and barrel_pcts[1][0] < 0.1:
-        return [0, 0, 0,0]
+        return [0, 0, 0,0,0,0, 0]
     elif barrel_pcts[0][0] > barrel_pcts[1][0]:
         return barrel_pcts[0]
     else:
@@ -442,10 +516,10 @@ def get_player_ids(player_names, player_map_df, team_name):
     return player_ids
 
 
-def get_hitter_outcomes(player_id, start_date, end_date):
+def get_hitter_outcomes(player_id, start_date, end_date, side):
     mapping = {'single': 1, 'double': 2, 'triple': 3, 'home_run': 4}
     player_outcomes = statcast_batter(start_dt=start_date, end_dt=end_date,player_id=player_id)
-    player_outcomes = player_outcomes[player_outcomes['events'].notna()]
+    player_outcomes = player_outcomes[(player_outcomes['events'].notna()) & (player_outcomes['stand'] == side)]
     player_outcomes['bases'] = player_outcomes['events'].map(lambda x: mapping.get(x, 0))
     if len(player_outcomes):
         print(player_outcomes['player_name'].iloc[0])
@@ -477,3 +551,94 @@ def update_hitter_history(player_hitting_history, start_date, end_date):
         player_hitting_history = pd.merge(player_hitting_history, new_data, on='date', how='left')
         player_hitting_history[player_id] = player_hitting_history[player_id].fillna(0)
     return player_hitting_history
+
+def get_hitter_barrels(hitter_barrels, hitter_ids, start_date, end_date, pitch_mix, sides):
+    if hitter_ids is None or len(hitter_ids) == 0:
+        return {}
+    hitter_barrels_copy = hitter_barrels[hitter_barrels['player_id'].isin(hitter_ids)].copy()
+    hitter_barrels_copy['barrel_data'] = hitter_barrels_copy['player_id'].apply(hitter_barrel_percentage, start_date=start_date, end_date=end_date, pitch_mix=pitch_mix, sides=sides)
+    hitter_barrels_copy['barrel_percentage'] = hitter_barrels_copy.apply(lambda row: get_barrel_pcts(row['barrel_data']), axis=1)
+    hitter_barrels_copy['barrel_qual'] = hitter_barrels_copy['barrel_percentage'].apply(lambda x: check_barrel_pcts(x))
+    # print(hitter_barrels_copy['barrel_qual'])
+    filtered = hitter_barrels_copy[hitter_barrels_copy['barrel_qual'].apply(lambda x: x != [0,0,0,0,0,0,0])].copy()
+    # print(filtered)
+    if len(filtered) > 3:
+        # Create a new column with just the first element
+        filtered['barrel_qual_first'] = filtered['barrel_qual'].apply(lambda x: x[0])
+        # Sort by that column and get top 3
+        top_3 = filtered.nlargest(3, 'barrel_qual_first')
+        # Drop the temporary column if you don't need it
+        top_3 = top_3.drop('barrel_qual_first', axis=1)
+        return dict(zip(zip(top_3['last_name, first_name'], top_3['player_id']), top_3['barrel_qual']))
+    else:
+        return dict(zip(zip(filtered['last_name, first_name'], filtered['player_id']), filtered['barrel_qual']))
+
+def get_hitter_barrels_date(hitter_barrels, hitter_ids, start_date, end_date, pitch_mix, sides, hitter_df):
+    if hitter_ids is None or len(hitter_ids) == 0:
+        return {}
+    hitter_barrels_copy = hitter_barrels[hitter_barrels['player_id'].isin(hitter_ids)].copy()
+    hitter_barrels_copy['barrel_data'] = hitter_barrels_copy['player_id'].apply(hitter_barrel_percentage_date, start_date=start_date, end_date=end_date, pitch_mix=pitch_mix, sides=sides, hitter_df=hitter_df)
+    hitter_barrels_copy['barrel_percentage'] = hitter_barrels_copy.apply(lambda row: get_barrel_pcts(row['barrel_data']), axis=1)
+    hitter_barrels_copy['barrel_qual'] = hitter_barrels_copy['barrel_percentage'].apply(lambda x: check_barrel_pcts(x))
+    # print(hitter_barrels_copy['barrel_qual'])
+    filtered = hitter_barrels_copy[hitter_barrels_copy['barrel_qual'].apply(lambda x: x != [0,0,0,0,0,0,0])].copy()
+    # print(filtered)
+    if len(filtered) > 3:
+        # Create a new column with just the first element
+        filtered['barrel_qual_first'] = filtered['barrel_qual'].apply(lambda x: x[0])
+        # Sort by that column and get top 3
+        top_3 = filtered.nlargest(3, 'barrel_qual_first')
+        # Drop the temporary column if you don't need it
+        top_3 = top_3.drop('barrel_qual_first', axis=1)
+        return dict(zip(zip(top_3['last_name, first_name'], top_3['player_id']), top_3['barrel_qual']))
+    else:
+        return dict(zip(zip(filtered['last_name, first_name'], filtered['player_id']), filtered['barrel_qual']))
+
+
+def get_specific_day_hitters(date, pitcher_df, hitter_df):
+    start_date = date.strftime('%Y-%m-%d')
+    end_date = (date + pd.DateOffset(days=-30)).strftime('%Y-%m-%d')    
+    pitcher_barrels = statcast_pitcher_exitvelo_barrels(2025)
+    
+    pitcher_ids = get_starting_ids_date(date)
+    pitcher_id_nos = [p[0] for p in pitcher_ids]
+    pitchers_arsenals = {}
+    pitcher_opponents = {}
+    pitcher_games = {}
+    for id, opposing_team, game_id in pitcher_ids:
+        pitcher_data = pitcher_df[id][(pitcher_df[id]['game_date'] <= end_date) & (pitcher_df[id]['game_date'] >= start_date)]
+        pitcher_opponents[id] = opposing_team
+        pitcher_games[id] = game_id
+        pitchers_arsenals[id] = {'R': get_pitch_arsenal(pitcher_data, 'R'), 'L': get_pitch_arsenal(pitcher_data, 'L')}
+
+    pitcher_barrels_copy = pitcher_barrels[pitcher_barrels['player_id'].isin(pitcher_id_nos)].copy()
+    pitcher_barrels_copy['barrel_percentage'] = pitcher_barrels_copy['player_id'].apply(get_barrel_percentage, start_date=start_date, end_date=end_date)
+    pitcher_barrels_copy['left_barrel_pct'] = pitcher_barrels_copy['barrel_percentage'].apply(lambda x: x['left_pct'])
+    pitcher_barrels_copy['right_barrel_pct'] = pitcher_barrels_copy['barrel_percentage'].apply(lambda x: x['right_pct'])
+    pitcher_barrels_copy['opponent'] = pitcher_barrels_copy['player_id'].apply(lambda x: pitcher_opponents[x])
+    pitcher_barrels_copy['game_id'] = pitcher_barrels_copy['player_id'].apply(lambda x: pitcher_games.get(x))
+    for index, row in pitcher_barrels_copy.iterrows():
+        if row['barrel_percentage']['total_pct'] == 0:
+            pitcher_barrels_copy.at[index, 'left_barrel_pct'] = row['brl_percent'] / 100
+            pitcher_barrels_copy.at[index, 'right_barrel_pct'] = row['brl_percent'] / 100
+        
+    filtered_pitcher_barrels_copy = pitcher_barrels_copy[(pitcher_barrels_copy['left_barrel_pct'] > 0.07) | (pitcher_barrels_copy['right_barrel_pct'] > 0.07)].copy()
+    filtered_pitcher_barrels_copy['sides'] = filtered_pitcher_barrels_copy.apply(lambda row: get_sides(row['left_barrel_pct'], row['right_barrel_pct']),axis=1)
+    filtered_pitcher_barrels_copy['arsenal'] = filtered_pitcher_barrels_copy.apply(lambda row: pitchers_arsenals[row['player_id']], axis=1)
+
+
+    pitcher_df = filtered_pitcher_barrels_copy.copy()
+    pitcher_df['hitter_team'] = pitcher_df.apply(lambda row: get_team(row['game_id'], row['opponent']), axis=1)
+    pitcher_df['opposing_lineup'] = pitcher_df.apply(lambda row: get_opposing_lineup(row['game_id'], row['opponent']), axis=1)
+
+    hitter_barrels = statcast_batter_exitvelo_barrels(2025)
+
+    hitter_pitcher_df = pitcher_df.copy()
+    hitter_pitcher_df['hitter_barrels'] = hitter_pitcher_df.apply(lambda row: get_hitter_barrels_date(hitter_barrels, row['opposing_lineup'], start_date, end_date, row['arsenal'], row['sides'], hitter_df), axis=1)
+
+    new_batter_ids = []
+    for index, row in hitter_pitcher_df.iterrows():
+        for key in row['hitter_barrels']:
+            new_batter_ids.append(row['hitter_barrels'][key][1])
+    
+    return new_batter_ids
